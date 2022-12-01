ab6625fe0d0f7f54ea1a7eaca9e6be8a3397264d
MohaQasim <malmajmae@gmail.com>
2022-12-01 15:27:15 +0200

    right side of the Intro

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -3,7 +3,15 @@ import styles from './intro.module.scss'
 import Github from '../../img/github.png'
  import LinkedIn from '../../img/linkedin.png'
  import Instagram from '../../img/instagram.png'
+ import Vector1 from '../../img/Vector1.png'
+ import Vector2 from '../../img/Vector2.png'
+ import boy from '../../img/boy.png'
+ import thumbup from '../../img/thumbup.png'
+ import Crown from '../../img/crown.png'
+ import glassesimoji from '../../img/glassesimoji.png'
+
 import classNames from   'classnames'
+
 
 const Intro: React.FC= () => {
 
@@ -41,7 +49,9 @@ const Intro: React.FC= () => {
     </div> 
 
      <div className={styles.iRight}>
-      i am right sida
+      <img src={Vector1} alt="" />
+      <img src={Vector2} alt="" />
+      <img src={boy} alt="" />
      </div>
 
   </div>
